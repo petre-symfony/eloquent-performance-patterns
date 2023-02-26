db186f6979c4f7a1a7a885c5eae09437399f0a8f
petrero <petrero@gmail.com>
2023-02-26 13:31:16 +0200

    1.6 Automatically load the users relationship any time a company is loaded. We go from 16 users load from database to 816 users load from database, but memory usage only increase from 2MB previously to 4MB.   Memory usage doesn't indicate that something is dramatically wrong but we may see that now we loading 832 models in the page but we only show 16 users and 16 companies

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model {
     use HasFactory;
 
+    protected $with = ['users'];
+    
     public function users() {
         return $this->hasMany(User::class);
     }
