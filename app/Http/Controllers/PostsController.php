e1e0b720a9c9e0d554c02e0c2bc8899f708b99e9
petrero <petrero@gmail.com>
2023-02-26 16:59:50 +0200

    2.3 Only 1MB of memory is used now. The difference to 17Mb is quite large

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller {
     public function index() {
+        return 'Hello World';
+        
         $years = Post::with('author')
             ->latest('published_at')
             ->get()
