edab5fb4251a1b17e13d43554b0b17ba7e5d8f26
petrero <petrero@gmail.com>
2023-03-01 22:44:04 +0200

    5.2 Add status badges to the top of the page to show how many features request are in each step of the process

--- resources/views/features/index.blade.php
+++ resources/views/features/index.blade.php
@@ -21,6 +21,36 @@
     </x-slot>
 
     <main class="max-w-6xl mx-auto sm:px-6 lg:px-8 py-12">
+        <div class="mb-12 grid grid-cols-3 gap-8">
+            <div class="bg-orange-400 shadow rounded-lg flex items-center justify-between px-8 py-5">
+                <div class="flex items-center">
+                    <svg class="text-white fill-current w-8 h-8" viewBox="0 0 20 20">
+                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+                    </svg>
+                    <div class="ml-3 text-white font-medium">Requested</div>
+                </div>
+                <div class="text-white ml-3 text-2xl font-medium">{{ $statuses->requested }}</div>
+            </div>
+            <div class="bg-blue-500 shadow rounded-lg flex items-center justify-between px-8 py-5">
+                <div class="flex items-center">
+                    <svg class="text-white fill-current w-7 h-7" viewBox="0 0 20 20">
+                        <path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z" />
+                    </svg>
+                    <div class="ml-3 text-white font-medium">Planned</div>
+                </div>
+                <div class="text-white ml-3 text-2xl font-medium">{{ $statuses->planned }}</div>
+            </div>
+            <div class="bg-green-400 shadow rounded-lg flex items-center justify-between px-8 py-5">
+                <div class="flex items-center">
+                    <svg class="text-white fill-current w-8 h-8" viewBox="0 0 20 20">
+                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+                    </svg>
+                    <div class="ml-3 text-white font-medium">Completed</div>
+                </div>
+                <div class="text-white ml-3 text-2xl font-medium">{{ $statuses->completed }}</div>
+            </div>
+        </div>
+
         <div class="flex flex-col">
             <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
                 <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg">
