dd402f91e29435d072bd2f53cc1eb770a4d63795
petrero <petrero@gmail.com>
2023-03-04 20:39:40 +0200

    7.3 We want to check each keywords in isolation which means we need to wrap our orWhere conditions in a closure.

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,8 +69,10 @@ class User extends Authenticatable {
     public function scopeSearch($query, string $terms = null){
         collect(explode(' ', $terms))->filter()->each(function ($term) use ($query) {
             $term = '%'.$term.'%';
-            $query->where('first_name', 'like', $term)
-                ->orWhere('last_name', 'like', $term);
+            $query->where(function ($query) use ($term) {
+                $query->where('first_name', 'like', $term)
+                    ->orWhere('last_name', 'like', $term);
+            });
         });
     }
 }
