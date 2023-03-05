e4585dff620212d32adb33213d839c7b163b128b
petrero <petrero@gmail.com>
2023-03-05 21:29:37 +0200

    9.1 In previous lesson we added indexes to the company and users table to try to speed up our search query. However we learn that the query was unable to use those indexes. We were able to fix the first issue that prevented us by removing the wildcard prefix from our like query.
     However we still have to solve the second issue and that's our company subquery.
     In this lesson we gonna trying do that by exploring faster options than orWhereHas.
     We try a join instead. If refresh our page we see that helps a little. We only drop combined execution queries overall time from 553ms to 524ms. However i suspect that we are still not using last_name and first_name indexes. Let's copy paste the query in mysql workbench and run an explain on it. We can see that they are showed as possible keys but they still are not actually used in this query so unfortunatly this approach doesn't help. Until we are able to use our indexes our queries will not gonna be as fast as we want.

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,27 +68,25 @@ class User extends Authenticatable {
 
     public function scopeSearch($query, string $terms = null){
         if (config('database.default') === 'mysql' || config('database.default') === 'sqlite') {
+            $query->join('companies', 'companies.id', '=', 'users.company_id');
             collect(str_getcsv($terms, ' ', '"'))->filter()->each(function ($term) use ($query) {
                 $term = $term . '%';
                 $query->where(function ($query) use ($term) {
                     $query->where('first_name', 'like', $term)
                         ->orWhere('last_name', 'like', $term)
-                        ->orWhereHas('company', function ($query) use ($term) {
-                            $query->where('name', 'like', $term);
-                        });
+                        ->orWhere('companies.name', 'like', $term);
                 });
             });
         }
 
         if (config('database.default') === 'pgsql') {
+            $query->join('companies', 'companies.id', '=', 'users.company_id');
             collect(str_getcsv($terms, ' ', '"'))->filter()->each(function ($term) use ($query) {
                 $term = $term.'%';
                 $query->where(function ($query) use ($term) {
                     $query->where('first_name', 'ilike', $term)
                         ->orWhere('last_name', 'ilike', $term)
-                        ->orWhereHas('company', function ($query) use ($term) {
-                            $query->where('name', 'ilike', $term);
-                        });
+                        ->orWhere('companies.name', 'ilike', $term);
                 });
             });
         }
